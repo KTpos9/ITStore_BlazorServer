1a3663e4ec4989095c8188de5357e9fa5aa0bd20
kittiphop <kittiphopzheng@hotmail.com>
2022-11-01 22:17:05 +0700

    added sql data access library

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess
+    {
+        public IConfiguration _config;
+        public string ConnectionStringName { get; set; } = "Default";
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameter)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+            using(IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameter);
+                return data.ToList();
+            }
+        }
+        public async Task SaveData<T>(string sql, T parameter)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameter);
+            }
+        }
+    }
+}
