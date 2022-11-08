0fb16c51599faf6ac23447f972f39887b35f954b
kittiphop <kittiphopzheng@hotmail.com>
2022-11-08 19:40:35 +0700

    add cart data access

--- DataAccess/Data/CartData.cs
+++ DataAccess/Data/CartData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace DataAccess.Data
 {
     internal class CartData
     {
+        private readonly SqlDataAccess _db;
+        public CartData(SqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<List<Cart>> GetCart()
+        {
+            string sql = "select * from dbo.Member";
+            return _db.LoadData<Cart, dynamic>(sql, new { });
+        }
+        public Task InsertCart(Cart cart)
+        {
+            string sql = @"insert into dbo.Member () 
+                          values (@,);";
+            return _db.SaveData(sql, cart);
+        }
     }
 }
