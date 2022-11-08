4b3d607fe358eac35f258ef1f8f196541a670c6d
kittiphop <kittiphopzheng@hotmail.com>
2022-11-09 00:55:42 +0700

    add the display model

---
+++ Web_Project/Models/DisplayMemberModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Project.Models
+{
+    public class DisplayMemberModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage ="Password doesn't match, please try again")]
+        public string ConfirmPassword { get; set; }
+    }
+}
