3f1ce06a21f951fe89a909493aef0a4adc3d3c9b
Pakawat Srithong <niceiceuz@hotmail.com>
2022-11-09 11:48:47 +0700

    add member/product edit

---
+++ DataAccess/Data/ProductData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Project.DataModels;
+
+namespace DataAccess.Data
+{
+    public class ProductData
+    {
+        private readonly SqlDataAccess _db;
+        public ProductData(SqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<List<Product>> GetProduct()
+        {
+            string sql = "select * from dbo.Product";
+            return _db.LoadData<Product, dynamic>(sql, new { });
+        }
+        public Task InsertMember(Product product)
+        {
+            string sql = @"insert into dbo.Product () 
+                          values (@,);";
+            return _db.SaveData(sql, product);
+        }
+    }
+}
