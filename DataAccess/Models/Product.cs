eb8c6db884f17aec9b3bd9c40ca5a08a46509327
kittiphop <kittiphopzheng@hotmail.com>
2022-11-09 23:52:15 +0700

    added attribute

--- DataAccess/Models/Product.cs
+++ DataAccess/Models/Product.cs
@@ -2,6 +2,9 @@
 {
     public class Product
     {
-        
+        public string ProductImg { get; set; }
+        public string ProiductName { get; set; }
+        public string ProductDesc { get; set; }
+        public float ProductPrice { get; set; }
     }
 }
