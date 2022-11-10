5c1698be8485d0f162b724ec9b3abe6cd78f26cf
kittiphop <kittiphopzheng@hotmail.com>
2022-11-11 00:19:08 +0700

    fixed typo

--- DataAccess/Models/Product.cs
+++ DataAccess/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public string ProductImg { get; set; }
-        public string ProiductName { get; set; }
+        public string ProductName { get; set; }
         public string ProductDesc { get; set; }
         public float ProductPrice { get; set; }
     }
