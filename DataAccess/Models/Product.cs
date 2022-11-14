2be222432dc6b3ad5f76687b16b5139063f01828
Pakawat Srithong <niceiceuz@hotmail.com>
2022-11-14 17:41:06 +0700

    edit the data models

--- DataAccess/Models/Product.cs
+++ DataAccess/Models/Product.cs
@@ -2,6 +2,7 @@
 {
     public class Product
     {
+        public int ProductId { get; set; }
         public string ProductImg { get; set; }
         public string ProductName { get; set; }
         public string ProductDesc { get; set; }
