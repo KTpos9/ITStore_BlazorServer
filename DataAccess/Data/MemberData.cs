caf85e384d734f2ead0396a57c2eb2b31e85dd61
kittiphop <kittiphopzheng@hotmail.com>
2022-11-17 14:07:40 +0700

    add GetMemberByEmail method and some sql changes

--- DataAccess/Data/MemberData.cs
+++ DataAccess/Data/MemberData.cs
@@ -16,14 +16,19 @@ namespace DataAccess.Data
         }
         public Task<List<Member>> GetMember()
         {
-            string sql = "select * from dbo.Member";
+            string sql = "select * from dbo.Member2";
             return _db.LoadData<Member,dynamic>(sql,new {});
         }
         public Task InsertMember(Member member)
         {
-            string sql = @"insert into dbo.Member () 
-                          values (@,);";
+            string sql = @"insert into dbo.Member2 (FirstName, LastName, Email, Password, Role) 
+                          values (@FirstName, @LastName, @Email, @Password, @Role);";
             return _db.SaveData(sql,member);
+        }
+        public Task<Member> GetMemberByEmail(string email)
+        {
+            string sql = "select * from dbo.Member2 where Email=@Email";
+            return _db.LoadSingleData<Member, dynamic>(sql, new { Email = email });
         }
     }
 }
