43e922b716add92f8a3a2ba5ae3c962f3e1dc264
kittiphop <kittiphopzheng@hotmail.com>
2022-11-17 14:08:50 +0700

    added properties for member class

--- DataAccess/Models/Member.cs
+++ DataAccess/Models/Member.cs
@@ -3,5 +3,10 @@
     public class Member
     {
         public int MemberId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
     }
 }
