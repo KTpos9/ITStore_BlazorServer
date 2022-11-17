896061012320134e92db75f9c73d487cee1813a9
kittiphop <kittiphopzheng@hotmail.com>
2022-11-17 14:09:16 +0700

    added auth provider

---
+++ Web_Project/Authentication/CustomAuthProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace Web_Project.Authentication
+{
+    public class CustomAuthProvider : AuthenticationStateProvider
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+        public CustomAuthProvider(ProtectedSessionStorage sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+                var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+                if (userSession == null)
+                {
+                    return await Task.FromResult(new AuthenticationState(_anonymous));
+                }
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email,userSession.Email),
+                    new Claim(ClaimTypes.Name,userSession.FirstName),
+                    new Claim(ClaimTypes.Role,userSession.Role)
+                }, "CustomAuth"));
+                return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+            }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(_anonymous));
+            }
+        }
+        public async Task UpdateAuthenticationStateAsync(UserSession userSession)
+        {
+            ClaimsPrincipal claimsPrincipal;
+            if(userSession != null)
+            {
+                await _sessionStorage.SetAsync("UserSession", userSession);
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email,userSession.Email),
+                    new Claim(ClaimTypes.Name,userSession.FirstName),
+                    new Claim(ClaimTypes.Role,userSession.Role)
+                }));
+            }
+            else
+            {
+                await _sessionStorage.DeleteAsync("UserSession");
+                claimsPrincipal = _anonymous;
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }
+}
---
+++ Web_Project/Authentication/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Project.Authentication
+{
+    public class UserSession
+    {
+        public string Email { get; set;}
+        public string FirstName { get; set;}
+        public string Role { get; set;} 
+    }
+}
